f6f374aad6637b73a1abb36c31b88b7962518c0f
paridoshi <43905268+paridoshi@users.noreply.github.com>
2020-05-25 16:09:38 +0530

    Add files via upload

---
+++ newctrl.js
@@ -0,0 +1,96 @@
+
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems',FoundItemsDirective);
+
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+      myTitle: '@title',
+      onRemove: '&'
+    },
+    controller:FoundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+ function FoundItemsDirectiveController() {
+  var list = this;
+
+  list.removeItem = function (itemIndex) {
+   
+     list.items.splice(itemIndex,1);
+      };
+   
+}
+
+
+
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var menu = this;  
+  menu.returnObj={};
+  menu.narrowBtn= function(){
+
+	if(!menu.searchItem){
+		menu.foundItems=[];
+		return;
+		}
+	var temp=MenuSearchService.getMatchedMenuItems(menu.returnObj,menu.searchItem,function(){
+
+	menu.foundItems= menu.returnObj.foundItems;
+	console.log(menu.foundItems);
+	});
+};
+
+};
+
+
+
+
+MenuSearchService.$inject = ['$http','$filter'];
+function MenuSearchService($http,$filter) {
+  var self = this;
+
+  self.getMenu = function () {
+	//service.getMenu = function () {
+    var response = $http({
+      method: "GET",
+      url: ("https://davids-restaurant.herokuapp.com/menu_items.json"),
+    });
+
+    return response;
+  };
+
+	self.getMatchedMenuItems = function(returnObj,searchItem,callback){
+		var promise = self.getMenu();
+		promise.then(function (response) {
+			console.log(response.data);
+			var foundItems=[];
+			returnObj.foundItems=$filter('filter')(response.data.menu_items, searchItem);
+			//console.log(foundItems);
+			callback();
+			})
+				.catch( function(error) {
+					returnObj.foundItems=[];
+					console.log(error);
+					callback();
+				})
+
+  			};
+ 	
+ 	
+		};
+
+})();
